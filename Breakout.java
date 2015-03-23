3e5f94bcd24c1b46f3df3c4fce3efd822416b5da
kasp0021 <kasp0021@hotmail.com>
2015-03-23 12:47:31 +0100

    Main Game

--- Breakout.java
+++ Breakout.java
@@ -18,9 +18,11 @@ public class Breakout extends GameLoop{ //Options and setup. Launch this for the
 	public void paint(Graphics g) { //Display graphics on screen
 		d.clearRect(0,0,800,600); //Clear background
 		
-		d.drawRect(x,y,120,20); //Add to paddle object
+		d.drawRect(x,y,120,20); //The player box
 		
 		for(int i = 0; i < balls.length;i++) balls[i].display(d);
+		
+		for(int i = 0; i < blocks.length;i++) blocks[i].display(d);
 		
 		//Ovals and rects are not drawn from center, but from corner for some reason.
 		
