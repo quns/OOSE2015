a60d526d5de7c136e7b847159b19fa270cdbaafd
quns <qsahib12@student.aau.dk>
2015-04-22 10:18:28 +0200

    Update Block.java

--- Block.java
+++ Block.java
@@ -1,32 +1,55 @@
+import java.awt.Color;
 import java.awt.Graphics;
 
 public class Block {
-	
 	int xpos, ypos;
-	int upperY, lowerY;
 	int health;
 	int width;
 	int height;
 	
 	public Block(int x, int y) { //Constructor
+		this.health = 1;
 		this.xpos = x;
 		this.ypos = y;
-		this.upperY = ypos;
-		this.lowerY = ypos+20;
 		this.width = 40;
 		this.height = 20;
 	}
 	
 	public void update() {
-		
 		if(health < 0) {
-		xpos = 850;
-		ypos = 650;
+			this.xpos = 850;
+			this.ypos = 650;
 		}
 	}
 	
+	public void setPos(int x, int y) {
+		this.xpos = x;
+		this.ypos = y;
+	}
+	
 	public void display(Graphics d) { //d = layer it will be drawn on
-		d.drawRect(xpos, ypos, width, height); 
+		//d.drawRect(xpos, ypos, width, height);
+		if(health == 1) {
+			d.setColor(Color.lightGray);
+			d.fillRect(xpos, ypos, width, height);	
+		}
+		
+		else if(health == 2) {
+			d.setColor(Color.gray);
+			d.fillRect(xpos, ypos, width, height);	
+		}
+		
+		else if(health == 3) {
+			d.setColor(Color.darkGray);
+			d.fillRect(xpos, ypos, width, height);	
+		}
+		
+		else {
+			d.setColor(Color.black);
+			d.fillRect(xpos, ypos, width, height);
+			this.xpos = 850;
+			this.ypos = 650;
+		}
 	}
 
 }
