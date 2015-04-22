d35afe1810f35cb85fec5e09c2cc5f334466d7da
Nevarin <nwojci12@student.aau.dk>
2015-04-22 10:17:17 +0200

    Update Ball.java

--- Ball.java
+++ Ball.java
@@ -1,4 +1,5 @@
 import java.applet.Applet;
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
 
@@ -36,38 +37,66 @@ public class Ball {
 	private void UpdateCollision() { //NEW CODE. ALL THE NUMBERS IN IF HAVE BEEN REPLACED
 		
 		for(int i = 0; i < GameLoop.blocks.length;i++) { //Block collision top
-			if(xpos < GameLoop.blocks[i].xpos+35 && xpos > GameLoop.blocks[i].xpos-5 && ypos > GameLoop.blocks[i].ypos-20 && ypos < GameLoop.blocks[i].ypos+18 ) {//Left collision
+			if(xpos + diameter > GameLoop.blocks[i].xpos && xpos < GameLoop.blocks[i].xpos+GameLoop.blocks[i].width && ypos + diameter > GameLoop.blocks[i].ypos && ypos < GameLoop.blocks[i].ypos + GameLoop.blocks[i].height) {
 				speedY *= -1;
 				GameLoop.blocks[i].health--;
-				//TEST
+				if(GameLoop.blocks[i].health == 0) GameLoop.blockCount--;
+				
 				if(GameLoop.powerupsCount < 5) { //Add it so they randomly spawn instead of every time
-				GameLoop.powerups[GameLoop.powerupsCount].setPos(GameLoop.blocks[i].xpos,GameLoop.blocks[i].ypos);
-				GameLoop.powerups[GameLoop.powerupsCount].alive = 1;
-				GameLoop.powerups[GameLoop.powerupsCount].newPower();
+					if(GameLoop.powerups[0].alive == 0) {
+						GameLoop.powerups[0].setPos(GameLoop.blocks[i].xpos,GameLoop.blocks[i].ypos);
+						GameLoop.powerups[0].alive = 1;
+						GameLoop.powerups[0].newPower();
+					}
+					else if(GameLoop.powerups[1].alive == 0) {
+						GameLoop.powerups[1].setPos(GameLoop.blocks[i].xpos,GameLoop.blocks[i].ypos);
+						GameLoop.powerups[1].alive = 1;
+						GameLoop.powerups[1].newPower();
+					}
+					else if(GameLoop.powerups[2].alive == 0) {
+						GameLoop.powerups[2].setPos(GameLoop.blocks[i].xpos,GameLoop.blocks[i].ypos);
+						GameLoop.powerups[2].alive = 1;
+						GameLoop.powerups[2].newPower();
+					}
+					else if(GameLoop.powerups[3].alive == 0) {
+						GameLoop.powerups[3].setPos(GameLoop.blocks[i].xpos,GameLoop.blocks[i].ypos);
+						GameLoop.powerups[3].alive = 1;
+						GameLoop.powerups[3].newPower();
+					}
+					else if(GameLoop.powerups[4].alive == 0) {
+						GameLoop.powerups[4].setPos(GameLoop.blocks[i].xpos,GameLoop.blocks[i].ypos);
+						GameLoop.powerups[4].alive = 1;
+						GameLoop.powerups[4].newPower();
+					}
 				}
 			}
 		}
 		
 		for(int i = 0; i < GameLoop.blocks.length;i++) { //Block collision Right
-			if(xpos < GameLoop.blocks[i].xpos+45 && xpos > GameLoop.blocks[i].xpos+40 && ypos > GameLoop.blocks[i].ypos-20 && ypos < GameLoop.blocks[i].ypos+18 ) {//Left collision
+			if(xpos > GameLoop.blocks[i].xpos+GameLoop.blocks[i].width && xpos < GameLoop.blocks[i].xpos+GameLoop.blocks[i].width+5 && ypos + diameter > GameLoop.blocks[i].ypos && ypos < GameLoop.blocks[i].ypos + GameLoop.blocks[i].height) {//Left collision
 				speedX *= -1;
 				GameLoop.blocks[i].health--;
+				if(GameLoop.blocks[i].health == 0) GameLoop.blockCount--;
 			}
 		}
 		
 		for(int i = 0; i < GameLoop.blocks.length;i++) { //Block collision Left
-			if(xpos < GameLoop.blocks[i].xpos-5 && xpos > GameLoop.blocks[i].xpos && ypos > GameLoop.blocks[i].ypos-20 && ypos < GameLoop.blocks[i].ypos+18 ) {//Left collision
+			if(xpos > GameLoop.blocks[i].xpos && xpos < GameLoop.blocks[i].xpos-5 && ypos + diameter > GameLoop.blocks[i].ypos && ypos < GameLoop.blocks[i].ypos + GameLoop.blocks[i].height) {//Left collision
 				speedX *= -1;
 				GameLoop.blocks[i].health--;
+				if(GameLoop.blocks[i].health == 0) GameLoop.blockCount--;
 			}
 		}
 		
-		if (xpos < 1 || xpos > 780) { //Reverse direction if ball hits sides
+		if (xpos < 0 || xpos > 780) { //Reverse direction if ball hits sides
 			speedX *= -1;
+			if(xpos < 0) xpos = 0;
+			if(xpos > 800) xpos = 780;
 		}
 		
 		if (ypos < 0) { //Reverse direction if ball hits top
 			speedY *= -1;
+			ypos = 0;
 		}
 		
 		if(ypos > 600) { //Reset ball if it hits bottom
@@ -75,27 +104,27 @@ public class Ball {
 			alive = 0;
 		}
 		
-		if(checkCollision == true) { //NEW CODE PADDLEWIDTH + HEIGHT
-			if(xpos < GameLoop.x+GameLoop.paddlewidth && xpos > GameLoop.x && ypos > GameLoop.y-GameLoop.paddleheight && ypos < GameLoop.y+GameLoop.paddleheight-2 ) { //Paddle detection top
+		if(checkCollision == true) { //Update
+			if(xpos + diameter > GameLoop.x && xpos < GameLoop.x+GameLoop.paddlewidth && ypos + diameter > GameLoop.y && ypos < GameLoop.y + GameLoop.paddleheight) { //Paddle detection top
 				speedY *= -1;
-				//NEW CODE
+
 				speedX += ((xpos-GameLoop.x)-(0.5f * GameLoop.paddlewidth))/50f; //Vary
+				
 				if (speedX > 5) speedX = 5; //Clamp speed
 				if (speedX < -5) speedX = -5;
-				// END NEW CODE
+
 				checkCollision = false;
 				start = System.currentTimeMillis();
-				
-			} //Right now the balls path is predetermined. Add some variation based on the balls hit on the paddle.
-			
-			if((xpos < GameLoop.x+GameLoop.paddlewidth+10 && xpos > GameLoop.x+GameLoop.paddlewidth && ypos > GameLoop.y-GameLoop.paddleheight && ypos < GameLoop.y+GameLoop.paddleheight-2 && speedX <0)) { //Paddle detection right
+			} 
+			//Update
+			if(xpos > GameLoop.x+GameLoop.paddlewidth && xpos < GameLoop.x+GameLoop.paddlewidth+5 && ypos + diameter > GameLoop.y && ypos < GameLoop.y + GameLoop.paddleheight && speedX < 0) { //Paddle detection right
 				speedY *= -1;
 				speedX *= -1;
 				checkCollision = false; //Disable collisions for a second afterwards to eliminate weird collisions
 				start = System.currentTimeMillis();
 			}
-			
-			if((xpos > GameLoop.x-15 && xpos < GameLoop.x && ypos > GameLoop.y-GameLoop.paddleheight && ypos < GameLoop.y+GameLoop.paddleheight-2 && speedX >0)) { //Paddle detection left
+			//Update
+			if(xpos > GameLoop.x && xpos < GameLoop.x-5 && ypos + diameter > GameLoop.y && ypos < GameLoop.y + GameLoop.paddleheight && speedX > 0) { //Paddle detection left
 				speedY *= -1;
 				speedX *= -1;
 				checkCollision = false; //Disable collisions for a second afterwards to eliminate weird collisions
@@ -103,7 +132,7 @@ public class Ball {
 			}
 		}
 		
-		if(checkCollision == false) { //Reactivate checkCollision in 1 second. Delay
+		if(checkCollision == false) { //Reactivate checkCollision in 1 second. Prevents the ball from hitting multiple hitboxes. Delay
 			long elapsedTime = System.currentTimeMillis() - start;
 			if(elapsedTime > 1000) checkCollision = true;
 		}
@@ -111,7 +140,10 @@ public class Ball {
 	}
 	
 	public void display(Graphics d) { //d = layer it will be drawn on
-		d.drawOval(xpos, ypos, diameter,diameter); 
+		//d.drawOval(xpos, ypos, diameter,diameter); 
+		d.setColor(Color.gray);
+		d.fillOval(xpos, ypos, diameter,diameter);
 	}
 	
 }
+
