2a87d6ead0ce0c76edf096b8a54e85e0e0ee23dd
Nevarin <nwojci12@student.aau.dk>
2015-05-07 12:15:08 +0200

    Update Block.java

--- Block.java
+++ Block.java
@@ -2,34 +2,33 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 public class Block {
-	int xpos, ypos;
-	int health;
-	int width;
-	int height;
+	int xpos, ypos; //Block x,y position
+	int health; //Block health
+	int width; //Block width
+	int height; //block height
 	
 	public Block(int x, int y) { //Constructor
-		this.health = 1;
+		this.health = 1; //Default health 1
 		this.xpos = x;
 		this.ypos = y;
-		this.width = 40;
-		this.height = 20;
+		this.width = 40; //Default width 40
+		this.height = 20; //Default height 20
 	}
 	
 	public void update() {
-		if(health < 0) {
+		if(health < 0) { //If block is dead, move outside of active playing field
 			this.xpos = 850;
 			this.ypos = 650;
 		}
 	}
 	
-	public void setPos(int x, int y) {
+	public void setPos(int x, int y) { //Manually set x,y position
 		this.xpos = x;
 		this.ypos = y;
 	}
 	
-	public void display(Graphics d) { //d = layer it will be drawn on
-		//d.drawRect(xpos, ypos, width, height);
-		if(health == 1) {
+	public void display(Graphics d) { //Display the block
+		if(health == 1) { //Changes the color of the block depending on how much life it has.
 			d.setColor(Color.lightGray);
 			d.fillRect(xpos, ypos, width, height);	
 		}
@@ -44,7 +43,7 @@ public class Block {
 			d.fillRect(xpos, ypos, width, height);	
 		}
 		
-		else {
+		else { //If the block is dead, make it black and force it outside of the playing field. Failsafe.
 			d.setColor(Color.black);
 			d.fillRect(xpos, ypos, width, height);
 			this.xpos = 850;
